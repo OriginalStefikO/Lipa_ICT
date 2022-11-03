71664d97f796cd317e2be537d1de431493d8e76e
OriginalStefikO <69142073+OriginalStefikO@users.noreply.github.com>
2022-11-03 23:05:02 +0100

    workingTransition

--- 20221102_js/js/script.js
+++ 20221102_js/js/script.js
@@ -6,31 +6,45 @@ window.addEventListener("load", function () {
     var posLeft = parseInt(pStyle.left);
     var posTop = parseInt(pStyle.top);
 
+    var waiting = false;
+
+    const jump = 25;
+    
     document.onkeydown = function (event) {
+        
+        if (waiting) { return; }
 
         switch (event.key) {
             case "ArrowLeft":
                 // Left pressed
                 console.log("Left pressed");
-                posLeft -= 10;
+                posLeft -= jump;
                 break;
             case "ArrowRight":
                 // Right pressed
                 console.log("Right pressed");
-                posLeft += 10;
+                posLeft += jump;
                 break;
             case "ArrowUp":
                 // Up pressed
                 console.log("Up pressed");
-                posTop -= 10;
+                posTop -= jump;
                 break;
             case "ArrowDown":
                 // Down pressed
                 console.log("Down pressed");
-                posTop += 10;
+                posTop += jump;
                 break;
         }
+        
+        waiting = true;
+        const myTimeout = setTimeout(waitingSwitch, parseFloat(pStyle.transitionDuration) * 1000);
+
         player.style.left = posLeft + "px";
         player.style.top = posTop + "px";
     };
+
+    function waitingSwitch() {
+        waiting = false;
+    }
 });
