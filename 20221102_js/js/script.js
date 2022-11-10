8b74844cae5ffa1953b0efd1190ca910b42fc899
OriginalStefikO <69142073+OriginalStefikO@users.noreply.github.com>
2022-11-10 09:16:04 +0100

    20221109_poleDone

--- 20221102_js/js/script.js
+++ 20221102_js/js/script.js
@@ -14,22 +14,22 @@ window.addEventListener("load", function () {
             case "ArrowLeft":
                 // Left pressed
                 console.log("Left pressed");
-                posLeft -= 10;
+                posLeft -= 40;
                 break;
             case "ArrowRight":
                 // Right pressed
                 console.log("Right pressed");
-                posLeft += 10;
+                posLeft += 40;
                 break;
             case "ArrowUp":
                 // Up pressed
                 console.log("Up pressed");
-                posTop -= 10;
+                posTop -= 40;
                 break;
             case "ArrowDown":
                 // Down pressed
                 console.log("Down pressed");
-                posTop += 10;
+                posTop += 40;
                 break;
         }
         player.style.left = posLeft + "px";
