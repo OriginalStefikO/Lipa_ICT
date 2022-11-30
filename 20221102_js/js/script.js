ed6b1e55fd15e0b003e628710bee6bdf6c1c10a8
OriginalStefikO <69142073+OriginalStefikO@users.noreply.github.com>
2022-11-30 17:34:53 +0100

    fungujiciPoleSten_20221130

--- 20221102_js/js/script.js
+++ 20221102_js/js/script.js
@@ -1,71 +1,91 @@
-window.addEventListener("load", function () {
-    const player = document.getElementById("player");
-    var pStyle = getComputedStyle(player);
-    var posLeft = parseInt(pStyle.left);
-    var posTop = parseInt(pStyle.top);
-    const pSize = parseInt(pStyle.width);
-    
-    const border = document.getElementById("border");
-    var borderStyle = getComputedStyle(border);
-    var borderPosLeft = parseInt(borderStyle.left);
-    var borderPosTop = parseInt(borderStyle.top);
-    const borderSize = parseInt(borderStyle.width);
+const player = document.getElementById("player");
+var pStyle = getComputedStyle(player);
+var posLeft = parseInt(pStyle.left);
+var posTop = parseInt(pStyle.top);
+const pSize = parseInt(pStyle.width);
 
-    var waiting = false;
+const border = document.getElementById("border");
+var borderStyle = getComputedStyle(border);
+var borderPosLeft = parseInt(borderStyle.left);
+var borderPosTop = parseInt(borderStyle.top);
+const borderSize = parseInt(borderStyle.width);
 
-    var obj = [];
+var waiting = false;
 
-    for (var i = 0; i < borderSize/pSize; i++) {
-        var radek = [];
-        for (var j = 0; j < borderSize/pSize; j++) {
-            radek.push(0);
-        }
-        obj.push(radek);
+var pole = [
+    [0, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [0, 0, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 0, 0, 0, 0, 0, 0],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+];
+
+
+var playerPos = [0, 0];
+
+document.onkeydown = function (event) {
+
+    if (waiting == true) {return};
+
+    pole[playerPos[1]][playerPos[0]] = 0;
+
+    switch (event.key) {
+        case "ArrowLeft":
+            // Left pressed
+            console.log("Left pressed");
+            if (posLeft > borderPosLeft) {
+                if (pole[playerPos[1]][playerPos[0] - 1] == 1) { break; }
+
+                posLeft -= pSize;
+                playerPos[0] -= 1;
+            }
+            break;
+        case "ArrowRight":
+            // Right pressed
+            console.log("Right pressed");
+            if (posLeft < (borderPosLeft + borderSize - pSize)) {
+                if (pole[playerPos[1]][playerPos[0] + 1] == 1) { break; }
+                posLeft += pSize;
+                playerPos[0] += 1;
+            }
+            break;
+        case "ArrowUp":
+            // Up pressed
+            console.log("Up pressed");
+            if (posTop > borderPosTop) {
+                if (pole[playerPos[1] - 1][playerPos[0]] == 1) { break; }
+                posTop -= pSize;
+                playerPos[1] -= 1;
+            }
+            break;
+        case "ArrowDown":
+            // Down pressed
+            console.log("Down pressed");
+            if (posTop < (borderPosTop + borderSize - pSize)) {
+                if (pole[playerPos[1] + 1][playerPos[0]] == 1) { break; }
+
+                posTop += pSize;
+                playerPos[1] += 1;
+            }
+            break;
     }
-    console.log(obj);
 
-    document.onkeydown = function (event) {
+    pole[playerPos[1]][playerPos[0]] = "X";
+    console.clear();
+    console.table(pole);
 
-        if (waiting == true) {return};
+    waiting = true;
+    const myTimeout = setTimeout(waitingSwitch, parseFloat(pStyle.transitionDuration) * 1000);
 
-        switch (event.key) {
-            case "ArrowLeft":
-                // Left pressed
-                console.log("Left pressed");
-                if (posLeft > borderPosLeft) {
-                    posLeft -= pSize;
-                }
-                break;
-            case "ArrowRight":
-                // Right pressed
-                console.log("Right pressed");
-                if (posLeft < (borderPosLeft + borderSize - pSize)) {
-                    posLeft += pSize;
-                }
-                break;
-            case "ArrowUp":
-                // Up pressed
-                console.log("Up pressed");
-                if (posTop > borderPosTop) {
-                    posTop -= pSize;
-                }
-                break;
-            case "ArrowDown":
-                // Down pressed
-                console.log("Down pressed");
-                if (posTop < (borderPosTop + borderSize - pSize)) {
-                    posTop += pSize;
-                }
-                break;
-        }
-        waiting = true;
-        const myTimeout = setTimeout(waitingSwitch, parseFloat(pStyle.transitionDuration) * 1000);
+    player.style.left = posLeft + "px";
+    player.style.top = posTop + "px";
+};
 
-        player.style.left = posLeft + "px";
-        player.style.top = posTop + "px";
-    };
-
-    function waitingSwitch() {
-        waiting = false;
-    }
-});
+function waitingSwitch() {
+    waiting = false;
+}
