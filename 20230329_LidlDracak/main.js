5cf5fbbde9fc1f0a1fc98e232e01ad86e66493de
OriginalStefikO <69142073+OriginalStefikO@users.noreply.github.com>
2023-05-03 18:01:18 +0200

    Button a prejdeme na Svelte

--- 20230329_LidlDracak/main.js
+++ 20230329_LidlDracak/main.js
@@ -10,6 +10,35 @@ const weapons = {
    "Healing Wand": new Weapon("Healing Wand", "heal", 0, 10)
 }
 
+const mainButton = document.querySelector("#mainButton");
+mainButton.addEventListener("click", gameStart);
+
+function gameStart() {
+    console.log("Game started");
+
+    const inputs = document.querySelectorAll("input");
+    const jmeno = inputs[0].value;
+    const zivoty = inputs[1].value;
+    const mana = inputs[2].value;
+    const sila = inputs[3].value;
+
+    const player = new Character(jmeno, zivoty, mana, sila);
+    console.log(player.toString());
+
+    // create element with inputs based on weapons
+    const weaponDiv = document.querySelector("#weaponDiv");
+    for (const weapon in weapons) {
+        const input = document.createElement("input");
+        input.type = "radio";
+        input.name = "weapon";
+        input.value = weapon;
+        weaponDiv.appendChild(input);
+
+        const label = document.createElement("label");
+        label.innerHTML = weapon;
+        weaponDiv.appendChild(label);
+    }
+}
 // player.learnWeapon(weapons["Sword"]);
 // console.log(player.toString())
 // player.attack(monster, weapons["Sword"]);
